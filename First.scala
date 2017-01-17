b813fce8455e40e7b704e225c59b845bc4971aab
SHUB9914 <shubhamagrawal6666@gmail.com>
2017-01-17 07:49:57 -0800

    firstcommit

--- First.scala
+++ First.scala
@@ -1,24 +1,13 @@
-
-class person(val name:String, val age:Int) extends Ordered[person]
-{
-def compare(that:person)=if( (this.name).equals(that.name) ) 0 else 1      //if name will be same then it will return true
-
-}
-object First
-{
-def main(args:Array[String])
-{
-var x =new person("shubham",11)
-var y =new person("shubham",10)
-if((x<=y))
-{
-if(x.age==y.age) println("name and age both are same ")   
-else println("age is not same")
-}
-else {
-if(x.name.length==y.name.length) println("length is same but name is not same") else println("length and name both not same ")
-
-}
-
+class Person(val name: String, val age: Int) extends Ordered[Person]{ //here ordered trait program
+def compare(that : Person) = {
+	if(this.name == that.name)
+		this.age - that.age 
+	else
+		this.name.length - that.name.length 
 }
 }
+object Persons extends App{
+	val firstPerson = new Person("shubham",20)
+	val secondPerson = new Person("shubham",21)
+	println( firstPerson < econdPersono)
+	}
